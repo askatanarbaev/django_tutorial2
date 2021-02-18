f344e39ade108db7b3986a0958a29d1bb84a5c0f
askatanarbaev <askatanarbaevchik@gmail.com>
2021-02-18 11:27:04 +0600

    part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
+from .models import *
 
 
 def index(request):
-    return HttpResponse('Hello world. You are at the polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', locals())
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', locals())
+
+
+def results(request, question_id):
+    response = "You are looking  at the of question %s. "
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
